7325789e653a95afef04651fb3d883d12dc71bca
Javier Sanz Sáez <javiersse@gmail.com>
2022-10-12 09:09:05 +0200

    Move & archive function implemented

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import subprocess
 import argparse
+import tempfile
+import shutil
+import os
+import tarfile
 
 parser = argparse.ArgumentParser()
 parser.add_argument('group', type=str, help='The group from which to copy all folders')
@@ -11,7 +15,17 @@ destination = args.destination
 
 
 def move(group:str, destination:str) -> None:
-    pass
+    # First we need to locate all files from the group
+    group_files = subprocess.run(f'find $HOME/exercise -group {group}', shell=True, capture_output=True)
+    group_files_list = group_files.stdout.decode().split("\n")[:-1] # The last element is empty
+    print(group_files_list)
+    
+    with tempfile.TemporaryDirectory() as tmpdir: # We will archive the files to a temp folder to avoid external interference
+        with tarfile.open(f'{tmpdir}/{group}.tar', mode='a') as tar:
+            for file in group_files_list:
+                tar.add(file, arcname=file.split(r'/')[-1]) # This only works in UNIX systems (a.k.a. not Windows)
+        shutil.move(f"{tmpdir}/{group}.tar", destination)
+
 
 def checks(group:str)->None:
     # TODO: Sanitize group to avoid injection
@@ -21,7 +35,6 @@ def checks(group:str)->None:
 
     if group_list=='':
         raise Exception("Input group is not an existing group of the system. Please check it is correctly written")
-    
 
 if __name__ == "__main__":
     checks(group=group)
